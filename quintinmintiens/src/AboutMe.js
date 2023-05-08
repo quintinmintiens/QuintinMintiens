cfe29719377614b857a42478673f7ee3a1b43078
quintinmintiens <75444661+quintinmintiens@users.noreply.github.com>
2023-05-08 13:43:47 +0200

    Added cv

--- quintinmintiens/src/AboutMe.js
+++ quintinmintiens/src/AboutMe.js
@@ -53,7 +53,7 @@ function AboutMe() {
         <div className="text-center mb-10">
           <img src={require("./ProfilePic.jpg")} alt='Profile Picture' className='profile-pic rounded-full w-15 h-15 mx-auto'/>
           <p className={`text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto my-10 ${isAnimated ? "text-animated" : ""}`}>
-            I am a junior software developer based in Belgium. I am passionate about creating clean, efficient, and scalable code. With experience in web development, I am constantly learning new technologies and seeking to improve my skills.
+          Hey there, I'm Quintin Mintiens, a 21-year-old student at HOGENT studying computer science. I'm currently specializing in data science and artificial intelligence, and I'm already working as a junior software developer. I'm always hungry for knowledge and eager to learn new skills to stay ahead in the rapidly evolving tech industry. I'm excited to share my journey with you as I continue to grow and make an impact in the world of technology.
           </p>
         </div>
         <div className={`flex justify-center ${isAnimated ? "text-animated" : ""}`}>
