4879cc0f61271159a7c900fa284b4845a26d476d
Quintin Mintiens <quintin.mintiens@student.hogent.be>
2023-12-20 20:06:47 +0100

    Update AboutMe.js
    Age updated

--- quintinmintiens/src/AboutMe.js
+++ quintinmintiens/src/AboutMe.js
@@ -53,7 +53,7 @@ function AboutMe() {
         <div className="text-center mb-10">
           <img src={require("./ProfilePic.jpg")} alt='Profile Picture' className='profile-pic rounded-full w-15 h-15 mx-auto'/>
           <p className={`text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto my-10 ${isAnimated ? "text-animated" : ""}`}>
-          Hey there, I'm Quintin Mintiens, a 21-year-old student at HOGENT studying computer science. I'm currently specializing in data science and artificial intelligence, and I'm already working as a junior software developer. I'm always hungry for knowledge and eager to learn new skills to stay ahead in the rapidly evolving tech industry. I'm excited to share my journey with you as I continue to grow and make an impact in the world of technology.
+          Hey there, I'm Quintin Mintiens, a 22-year-old student at HOGENT studying computer science. I'm currently specializing in data science and artificial intelligence, and I'm already working as a junior software developer. I'm always hungry for knowledge and eager to learn new skills to stay ahead in the rapidly evolving tech industry. I'm excited to share my journey with you as I continue to grow and make an impact in the world of technology.
           </p>
         </div>
         <div className={`flex justify-center ${isAnimated ? "text-animated" : ""}`}>
